a3fe032743570f5cddd0f4a4631ada4494f675a0
Austin Papritz <austin@papritz.dev>
2023-07-12 10:05:02 -0700

    file rearranging

--- RockPaperScissors.Tests/ModelTests/UnitTest1.cs
+++ RockPaperScissors.Tests/ModelTests/UnitTest1.cs
@@ -1,3 +1,8 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RockPaperScissors.Models;
+using System.Collections.Generic;
+using System;
+
 namespace RockPaperScissors.Tests;
 
 [TestClass]
---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace RockPaperScissors
+class Program
+{
+    static void Main()
+    {
+        // Code goes here.
+    }
+}
